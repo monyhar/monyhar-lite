6c27bac6a4d8abeadca1e23ea4af0aaeb8340daf
Yufan You <ouuansteve@gmail.com>
2021-06-13 20:38:43 +0800

    fix: project name "monyhar" -> "monyhar-lite"

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-monyhar: main.o
-	gcc main.o -o monyhar
+monyhar-lite: main.o
+	gcc main.o -o monyhar-lite
 
 main.o:
 	gcc -c main.c -o main.o
--- main.c
+++ main.c
@@ -2,23 +2,22 @@
 #include <string.h>
 
 int surf_internet(const char *url) {
-  if (strcmp(url, "https://www.monyhar.com/") == 0) {
+  if (strcmp(url, "https://www.monyhar.com/lite") == 0) {
     printf("梦弘浏览器 自主研发版本 - 完全自主研发，打破国外垄断，比 Chrome 快 600%%。\n");
     return 0;
   }
-  printf("连接超时\n"
-      "%s 的服务器响应时间过长。\n"
-      "\n"
+  printf("连接超时\n\n"
+      "%s 的服务器响应时间过长。\n\n"
       "- 此站点暂时无法使用或者太过忙碌。请过几分钟后再试。\n"
       "- 如果您无法载入任何网页，请检查您计算机的网络连接状态。\n"
-      "- 如果您的计算机或网络受到防火墙或者代理服务器的保护，请确认 monyhar 已被授权访问网络。\n"
+      "- 如果您的计算机或网络受到防火墙或者代理服务器的保护，请确认 monyhar-lite 已被授权访问网络。\n"
       , url);
   return 2;
 }
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "使用方法: monyhar <url>\n");
+    fprintf(stderr, "使用方法: monyhar-lite <url>\n");
     return 1;
   }
   return surf_internet(argv[1]);
