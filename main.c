a5e1a2f0ebdd834889f90dc4805a9c70c5fe27cf
Yufan You <ouuansteve@gmail.com>
2021-06-13 20:24:27 +0800

    feat: add 上网功能
    所谓浏览器，就是可以通过网址获取网站内容的工具。
    众所周知，一个浏览器不需要能够访问互联网上所有网站。

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int surf_internet() {
-  return 0;
+int surf_internet(const char *url) {
+  if (strcmp(url, "https://www.monyhar.com/") == 0) {
+    printf("梦弘浏览器 自主研发版本 - 完全自主研发，打破国外垄断，比 Chrome 快 600%%。\n");
+    return 0;
+  }
+  printf("连接超时\n"
+      "%s 的服务器响应时间过长。\n"
+      "\n"
+      "- 此站点暂时无法使用或者太过忙碌。请过几分钟后再试。\n"
+      "- 如果您无法载入任何网页，请检查您计算机的网络连接状态。\n"
+      "- 如果您的计算机或网络受到防火墙或者代理服务器的保护，请确认 monyhar 已被授权访问网络。\n"
+      , url);
+  return 2;
 }
 
-int main() {
-  return surf_internet();
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "使用方法: monyhar <url>\n");
+    return 1;
+  }
+  return surf_internet(argv[1]);
 }
