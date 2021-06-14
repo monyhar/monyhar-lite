c9caeb659d0d89f625e43945b852899485461bb4
Yufan You <ouuansteve@gmail.com>
2021-06-14 10:06:21 +0800

    feat: 既然会超时，就应该有加载用时

--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+// sleep
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
 int surf_internet(const char *url) {
+  printf("\n加载中.");
+
   if (strcmp(url, "https://www.monyhar.com/lite") == 0) {
-    printf("\n梦弘浏览器 自主研发版本 - 完全自主研发，打破国外垄断，比 Chrome 快 600%%。\n\n");
+    printf("\n\n梦弘浏览器 自主研发版本 - 完全自主研发，打破国外垄断，比 Chrome 快 600%%。\n\n");
     return 0;
   }
-  printf("\n连接超时\n\n"
+
+  for (int i = 0; i < 9; ++i) {
+    printf(".");
+    fflush(stdout);
+#ifdef _WIN32
+    Sleep(500);
+#else
+    usleep(500000);
+#endif
+  }
+
+  printf("\n\n连接超时\n\n"
       "%s 的服务器响应时间过长。\n"
       "- 此站点暂时无法使用或者太过忙碌。请过几分钟后再试。\n"
       "- 如果您无法载入任何网页，请检查您计算机的网络连接状态。\n"
