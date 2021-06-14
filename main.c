afabb0016cb710fa16ef6ad4b94ba9fa6ca298f7
Yufan You <ouuansteve@gmail.com>
2021-06-14 09:55:23 +0800

    feat: 好的浏览器应当能够切换当前浏览的网址，而不是只能获取一个网页

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 
 int surf_internet(const char *url) {
   if (strcmp(url, "https://www.monyhar.com/lite") == 0) {
-    printf("梦弘浏览器 自主研发版本 - 完全自主研发，打破国外垄断，比 Chrome 快 600%%。\n");
+    printf("\n梦弘浏览器 自主研发版本 - 完全自主研发，打破国外垄断，比 Chrome 快 600%%。\n\n");
     return 0;
   }
-  printf("连接超时\n\n"
-      "%s 的服务器响应时间过长。\n\n"
+  printf("\n连接超时\n\n"
+      "%s 的服务器响应时间过长。\n"
       "- 此站点暂时无法使用或者太过忙碌。请过几分钟后再试。\n"
       "- 如果您无法载入任何网页，请检查您计算机的网络连接状态。\n"
-      "- 如果您的计算机或网络受到防火墙或者代理服务器的保护，请确认 monyhar-lite 已被授权访问网络。\n"
+      "- 如果您的计算机或网络受到防火墙或者代理服务器的保护，请确认 monyhar-lite 已被授权访问网络。\n\n"
       , url);
-  return 2;
+  return 1;
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "使用方法: monyhar-lite <url>\n");
-    return 1;
+int main() {
+  char url[10000];
+  while (printf("请输入网址: ") && ~scanf("%s", url)) {
+    surf_internet(url);
   }
-  return surf_internet(argv[1]);
+  return 0;
 }
