ede0d0e5553b039fc210336265a1d6bc315cbf5f
yiboot <85206810+yiboot@users.noreply.github.com>
2021-06-16 11:19:15 +0800

    全面兼容小米平台
    大佬们支持了鸿蒙，那也应该兼容小米平台。

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <windows.h>
 
 int surf_internet() {
   return 0;
 }
 
+void xiaomi_internet(){
+  printf("兼容小米平台,30秒后启动，<点击跳过广告>");
+}
+
 int main() {
+  xiaomi_internet();
+  Sleep(3000);
   return surf_internet();
 }
