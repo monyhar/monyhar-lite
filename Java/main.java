fe3d466e925c8d97e5b98fdb5549990116003927
onlyGuo <38489049+onlyGuo@users.noreply.github.com>
2021-06-18 15:16:02 +0800

    fix the syntax error！
    修正语法错误。

--- Java/main.java
+++ Java/main.java
@@ -1,5 +1,8 @@
-public class surf_internet{
+public class Main{
     public static void main(String[] args){
+        surf_internet();
+    }
+    public static int surf_internet(){
         return 0;
     }
 }
