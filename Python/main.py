1d28c8838d38aa7cb6bfb0c06b4d7f39bbe8cd5e
tucaoba2333 <60278775+tucaoba2333@users.noreply.github.com>
2021-06-18 21:47:18 +0800

    整合了一行版本与main.py，使main.py可以获取页面信息.

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,10 @@
 def surf_internet():
+    (lambda req, url: req.urlopen(req.Request(url, headers={'User-Agent': 'monyhar/0.0.2'})).read())(
+        __import__('urllib.request').request, input('请输入网址：'))
     return 0
 
 
 surf_internet()
-#H A R M O N E Y
-#MONEY MONEY MONEY,MONEY MONEY(自行脑补屑女仆
+
+# H A R M O N E Y
+# MONEY MONEY MONEY,MONEY MONEY(自行脑补屑女仆
