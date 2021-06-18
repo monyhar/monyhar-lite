fb638bf83220bc87755cd0b9e323826a6daf64f3
tucaoba2333 <60278775+tucaoba2333@users.noreply.github.com>
2021-06-17 21:13:17 -0700

    nice.

---
+++ Python/main.py
@@ -0,0 +1,7 @@
+def surf_internet():
+    return 0
+
+
+surf_internet()
+#H A R M O N E Y
+#MONEY MONEY MONEY,MONEY MONEY(自行脑补屑女仆
--- main.java
+++
@@ -1,5 +0,0 @@
-public class surf_internet{
-    public static void main(String[] args){
-        return 0;
-    }
-}
