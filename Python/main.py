f1bc077c98e993bdaca2c0b4f7ad69734bd16e12
tucaoba2333 <60278775+tucaoba2333@users.noreply.github.com>
2021-06-19 19:37:13 +0800

    今天起飞了

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,12 @@
+import os, sys
+import requests
 from typing import Dict, Any
 
-import requests
+# U know the rules,
+from sqlmap.thirdparty.bottle.bottle import url
+
+# And so do I~
+
 
 if_proxy = input("Do you want to set proxy server?[Y/n]")
 
@@ -9,22 +15,41 @@ if if_proxy == "Y":
     https_proxy = input("Type https proxy address here.")
 
     proxies: dict[str, Any] = {"http": http_proxy, "https": https_proxy}
-
+    if_test = input("Test the proxy server?[Y/n]")
+    if if_test == "Y":
+        ping_http_proxy = "ping" + http_proxy
+        ping_https_proxy = "ping" + https_proxy
+        ping_result = os.system("ping " + http_proxy)
+        if "Lost = 0" in ping_result:
+            print("Connected to the http proxy server.")
+        ping_result = os.system("ping " + https_proxy)
+        if "Lost = 0" in ping_result:
+            print("Connected to the https proxy server.")
 
 else:
     print("No proxy server was set.")
 
 
-def surf_internet(url):
-    html = requests.get(url)
-    print(html.status_code)  # print the http code returned.
-    print(html.text)  # print text returned.
+class Monyhar:
+    def surf_internet(self):
+        html = requests.get(self, url)
+        print(html.status_code)  # print the http code returned.
+        print(html.text)  # print text returned.
+
+    @staticmethod
+    def about():
+        print("Monyhar Browser,made by tucaoba233.")
+        print("©CopyRight 2021-2021 tucaoba233, All Rights Reserved.")
 
 
-# After input url,please use 'surf_internet(url)' to surf internet
-
+global url
 url = input("url:")
 
-surf_internet(url)
+if "www." not in url:
+    url = "www." + url
+print("Auto inserted 'www.")
+if "http://" not in url:
+    url = "http://" + url
+print("Auto inserted 'http://'.")
 
-
+Monyhar.surf_internet(url)
