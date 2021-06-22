459a5ca21e8c61993222329fb287061a4839686a
susur <509183786@qq.com>
2021-06-22 14:19:42 +0800

    添加monyhar启动失败时重试功能

--- MonyharApplication.py
+++ MonyharApplication.py
@@ -6,8 +6,10 @@ def main():
         webbrowser.open('https://so.monyhar.top/', new=2, autoraise=True)
     except:
         print('Failed connect to the Internet!')
+        print('Start retrying now...')
+        main()
     else:
         print('Successfully connect to the Internet!')
-
+    
 if __name__ == "__main__":
     main()
