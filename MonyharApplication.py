b900370d5ae33f48c6e595bd9c2fec92212fb68e
susur <509183786@qq.com>
2021-06-22 15:07:48 +0800

    修复了无法设置首页的bug,添加了配置文件的功能,可以配置文件中方便的设置首页

--- MonyharApplication.py
+++ MonyharApplication.py
@@ -1,9 +1,24 @@
 
+from os import readlink
 import webbrowser
-
+import json
+'''
+配置参数:
+    homepage: 浏览器首页地址(默认为梦弘搜索器)
+'''
+HOMEPAGE = 'https://so.monyhar.top/'
+# 加载配置文件
+def getConfig():
+    global HOMEPAGE
+    with open('config/config.json', 'r') as f:
+        monyharConfig = json.load(f)
+    HOMEPAGE = monyharConfig['homepage']
+       
+# 主程序
 def main():
+    getConfig()
     try:
-        webbrowser.open('https://so.monyhar.top/', new=2, autoraise=True)
+        webbrowser.open(HOMEPAGE, new=1, autoraise=True)
     except:
         print('Failed connect to the Internet!')
         print('Start retrying now...')
@@ -13,3 +28,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+
