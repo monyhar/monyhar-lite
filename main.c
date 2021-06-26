427774844e892532833f36119f5040ce7949e557
luxi78 <luxi78@gmail.com>
2021-06-26 17:55:09 +0800

    Update main.c
    全世界最快的π 计算方法诞生了，计算到小数点后10亿位只要1秒钟，完全自主研发，秒杀一切国外算法。

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+
 
 int surf_internet() {
   return 0;
 }
 
+int oneBillionAfterTheDecimalPointOfPi() {
+  sleep(1);
+  return 3;
+}
+
 int main() {
+  printf("π小数点后10亿位是%d", oneBillionAfterTheDecimalPointOfPi());
   return surf_internet();
 }
