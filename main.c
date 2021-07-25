76ba98d53561fa1860490486d0da846df225cad2
cdn2021 <78522762+cdn2021@users.noreply.github.com>
2021-07-25 21:48:13 +0800

    实现上网功能
    实现上网功能

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
-
+#include <stdio.h> // printf,scanf 原型
+#include <stdlib.h> // malloc,free 原型
+#include <string.h> //strcat原型
 int surf_internet() {
+  while(1){
+    printf("URL:");
+    char * url = malloc(sizeof(char) * 1024); //1024个字符的url，应该够用了
+    if (scanf("%s",&url)){
+      //读取
+      char * command = malloc(sizeof(char) * 1029);
+      *command = "curl ";
+      strcat(command,url);//拼接
+      system(command);//命令行调用
+      free(command); //释放内存
+      printf("\n");
+      
+    };
+    free(url);
   return 0;
 }
 
